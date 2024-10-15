938c995f125dc2bcfebc454dc2fb7d1db3b2186c
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-10-15 21:54:11 +0300

    Connected to mongodb database and made index and about pages

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const express = require("express");
+const expressLayout = require('express-ejs-layouts');
+
+const connectDB = require('./server/config/db');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
-app.get('', (req, res) => {
-    res.send("Hello World");
-})
+// Connect to DB
+connectDB();
+
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'))
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
